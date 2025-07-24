eb4b3345e7545dcccb168d6cbbbbcfaf0e705f4e
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-07-24 18:27:59 +0545

    Added note view functionality

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-   <h2>edit</h2>
+   <div class="note-contanier">
+      <h1>Edit your notes</h1>
+      <form action="{{route('note.update', $note)}}" method="post" class="note">
+         <textarea name="note" rows="10" class="note-body" placeholder="Enter your notes here">
+            {{$note->note}}
+         </textarea>
+         <div class="note-buttons">
+            <a href="{{route('note.index')}}" class="note-cancle-button">Cancel</a>
+            <button class="note-sumbit-button">Submit</button>
+         </div>
+      </form>
+   </div>
 </x-layout>
