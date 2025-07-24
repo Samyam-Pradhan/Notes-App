412db75a9cbb7a279486cd9bf28a54b9eb18c576
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-07-24 19:52:48 +0545

    fix: update NoteController to show notes only for authenticated user

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -12,7 +13,9 @@ class NoteController extends Controller
      */
     public function index()
     {
-        $notes = Note::query()->orderBy("created_at","desc")->paginate(15);
+        $notes = Note::query()
+        ->where('user_id',request()->user()->id)
+        ->orderBy("created_at","desc")->paginate(15);
         return view("note.index",['notes'=> $notes]);
     }
 
@@ -33,7 +36,7 @@ class NoteController extends Controller
             'note' => ['required', 'string']
         ]);
 
-        $data['user_id'] = 1;
+        $data['user_id'] = $request->user()->id;
         $note = Note::create($data);
 
         return to_route('note.show', $note)->with('message', 'Note was created');
@@ -44,6 +47,8 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
+        if($note->user_id !== request()->user()->id);
+        abort(403);
         return view('note.show',['note'=> $note]);
     }
 
@@ -52,6 +57,8 @@ class NoteController extends Controller
      */
     public function edit(Note $note) //Note is the model class and $note is the variable
     {
+        if($note->user_id !== request()->user()->id);
+        abort(403);
         return view('note.edit',['note'=> $note]);
     }
 
@@ -60,7 +67,8 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        
+        if($note->user_id !== request()->user()->id);
+        abort(403);
         $data = $request->validate([
             'note' => ['required', 'string']
         ]);
@@ -75,6 +83,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
+        if($note->user_id !== request()->user()->id);
+        abort(403);
         $note->delete();
 
         return to_route('note.index', $note)->with('message','Note was deleted');
